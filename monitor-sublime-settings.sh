2a85a149974ea76e72247ef7d9bca523840cdb77
Sanni Abdulmusawwir <sanniabdulmusawwir@gmail.com>
2016-08-23 22:48:17 +0100

    update monitor script to display notifications when run as cron in ubuntu

--- monitor-sublime-settings.sh
+++ monitor-sublime-settings.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Add this so that notify-send would display the notification on Ubuntu
+eval "export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $LOGNAME gnome-session)/environ)";
+
 # @TODO: check if running on Ubuntu or OSX in order to use the appropriate directory and notifier
 #
 # This could come in handy: http://askubuntu.com/questions/459402/how-to-know-if-the-running-platform-is-ubuntu-or-centos-with-help-of-a-bash-scri
@@ -7,5 +10,5 @@
 
 CHANGES_EXIST=`git -C $HOME/.config/sublime-text-3/Packages/User/ status --porcelain`
 if [[ ${CHANGES_EXIST} ]]; then
-    notify-send -i terminal "Sublime Text 3" "Sublime settings have changed"
+    /usr/bin/notify-send -i terminal "Sublime Text 3" "Sublime settings have changed"
 fi
